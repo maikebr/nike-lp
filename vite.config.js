514e8166c14f01a0446299534aee81cd786cd824
Maike Muzitano <maikemuzitano@gmail.com>
2023-10-03 11:26:47 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://maikebr.github.io/nike-lp/",
+	base: "/nike-lp/",
 });
