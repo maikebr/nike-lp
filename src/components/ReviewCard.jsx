16ad5340ceb31c94cfc7ad3314dbbe4dbe151599
Maike Muzitano <maikemuzitano@gmail.com>
2023-10-04 17:17:53 -0300

    reviews

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,27 @@
+import { star } from "../assets/icons";
+
+const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
+	return (
+		<div className="flex justify-center items-center flex-col">
+			<img
+				src={imgURL}
+				alt="costumer"
+				className="rounded-full object-cover w-[120px] h-[120px]"
+			/>
+			<p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+			<div className="flex gap-2 mt-3 items-center justify-center">
+				<img
+					src={star}
+					alt="star"
+					className="object-contain m-0 h-[24px] w-[24px]"
+				/>
+				<p className="text-xl font-montserrat text-slte-gray">({rating})</p>
+			</div>
+			<h3 className="mt-1 font-palanquin text-3xl text-center font-bold">
+				{customerName}
+			</h3>
+		</div>
+	);
+};
+
+export default ReviewCard;
