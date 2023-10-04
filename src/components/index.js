3fa4c782b1a5700b8fa7e2d346022b789343b1f8
Maike Muzitano <maikemuzitano@gmail.com>
2023-10-04 16:07:39 -0300

    cards

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button";
 import ShoeCard from "./ShoeCard";
 import NavBar from "./NavBar";
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
 
-export { Button, ShoeCard, NavBar, PopularProductCard };
+export { Button, ShoeCard, NavBar, PopularProductCard, ServiceCard };
